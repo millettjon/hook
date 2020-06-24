0e129d0a1a6c9268a549129a6887421519b73c57
Jonathan Millett <jon@ewaydirect.com>
2020-06-24 18:44:02 -0400

    11214 - Cleaned up ns form.

--- eway/site/11214/subscriber_response_hook/cli.clj
+++ eway/site/11214/subscriber_response_hook/cli.clj
@@ -1,12 +1,14 @@
 (ns eway.site.11214.subscriber-response-hook.cli
   "Queries new subscriber responses for site 11214 (Canvaspeople) and
   calls the Jondo webhook for each."
-  (:require [fipp.edn :refer [pprint]]
-            [taoensso.timbre :refer [info warn]]
-            [eway.app :as app]
-            [eway.client.jondo.hook.subscriber-response :as hook]))
+  (:require[taoensso.timbre :refer [info]]
+           [eway.log :as el]
+           [eway.config :refer [read-config]]
+           [cli-matic.core :as cli]
+           [eway.client.jondo.hook.subscriber-response :as hook]))
 
-;; Template: https://sp.canvaspeople.com/i?e=se&p=srv&tv=no-js&se_ca=lead&se_ac={{ action }}&se_la={{ email }}&se_pr=subid-{{ text18 }}|rti-{{ rti }}&aid=cp_lead_tracker
+;; Canvas People Template:
+;; https://sp.canvaspeople.com/i?e=se&p=srv&tv=no-js&se_ca=lead&se_ac={{ action }}&se_la={{ email }}&se_pr=subid-{{ text18 }}|rti-{{ rti }}&aid=cp_lead_tracker
 ;; Example: https://sp.canvaspeople.com/i?e=se&p=srv&tv=no-js&se_ca=lead&se_ac=new&se_la=<redacted>@yahoo.com&se_pr=subid--1|rti-117410&aid=cp_lead_tracker
 ;; Note: For small batch testing with client, append _test to the :aid parameter.
 (def endpoint
@@ -20,24 +22,26 @@
             :se_pr [[:text18 :respondingtoid] #(str "subid-" %1 "|rti-" %2)]
             :aid   "cp_lead_tracker"}})
 
-(defn process
-  [config]
-  (hook/process-new-rows (assoc config
-                                :site-id 11214
-                                :endpoint endpoint)))
-
 (defn -main
-  [& _]
-  (app/main process))
+  [& args]
+  (let [config (assoc (read-config)
+                      :site-id 11214
+                      :endpoint endpoint)
+        cli-config (hook/build-cli config)]
+    (info :load-config config)
+    (el/init* config)
+    (cli/run-cmd args cli-config)))
 
 (comment
   (with-bindings {#'eway.config/*profile* :dev}
-    #_ (-main)
-    (process (eway.config/read-config))
-
-    #_ (hook/current-id (eway.config/read-config))
+    (let [config (assoc (read-config)
+                        :site-id 11214
+                        :endpoint endpoint)]
+      #_ (hook/set-id-to-max config)
+      (hook/set-id (eway.config/read-config) 42)
+      (hook/status config)
+      #_ (hook/process-new-rows config)
+      )
     #_ (hook/max-subscriber-response-id)
-    #_ (hook/set-id (eway.config/read-config) 42)
-    #_ (hook/set-id-to-max (eway.config/read-config))
     )
-)
+  )
